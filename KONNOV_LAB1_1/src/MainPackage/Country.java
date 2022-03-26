d2222a2554319baecfc053cedfe2325d189d767e
Odhinn3 <odhinn3@gmail.com>
2022-03-26 23:49:50 +0300

    Lab 1 part 2 (books) is complete

--- KONNOV_LAB1_1/src/MainPackage/Country.java
+++ KONNOV_LAB1_1/src/MainPackage/Country.java
@@ -1,3 +1,5 @@
+package MainPackage;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -28,6 +30,7 @@ public class Country {
         setPopul(popul);
         populdensity = popul/area;
     }
+    
     
     
     //конструктор для государств со столицей
--- KONNOV_LAB1_1/src/MainPackage/MainClass.java
+++ KONNOV_LAB1_1/src/MainPackage/MainClass.java
@@ -1,3 +1,5 @@
+package MainPackage;
+
 
 public class MainClass {
     public static void main(String[] args) {
@@ -9,6 +11,7 @@ public class MainClass {
         conArray[2] = new Country("France", 643800, 67800000, "Paris", 2100000);
         conArray[3] = new Country("Andorra", 467, 85400, "Andorra la Vella", 22600);
         conArray[4] = new Country("Singapore", 725, 5700000);
+        
         
         
         //вывод на печать данных массива
